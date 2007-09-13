ec07b4c550147f840fd78e9fdc11206da98c0c30
Casiano Rodriguez-Leon <casiano.rodriguez.leon@gmail.com>
2007-09-13 17:06:31 +0000

    Import of CASIANO/Parse-Eyapp-1.075 from CPAN.
    gitpan-cpan-distribution: Parse-Eyapp
    gitpan-cpan-version:      1.075
    gitpan-cpan-path:         CASIANO/Parse-Eyapp-1.075.tar.gz
    gitpan-cpan-author:       CASIANO
    gitpan-cpan-maturity:     released

--- lib/Parse/Eyapp/Treeregexp.pm
+++ lib/Parse/Eyapp/Treeregexp.pm
@@ -1,6 +1,6 @@
 ###################################################################################
 #
-#    This file was generated using Parse::Eyapp version 1.074.
+#    This file was generated using Parse::Eyapp version 1.075.
 #
 # (c) Parse::Yapp Copyright 1998-2001 Francois Desarmenien.
 # (c) Parse::Eyapp Copyright 2006 Casiano Rodriguez-Leon. Universidad de La Laguna.
@@ -231,7 +231,7 @@ sub new {
     and $class=ref($class);
 
     warn $warnmessage unless __PACKAGE__->isa('Parse::Eyapp::Driver'); 
-    my($self)=$class->SUPER::new( yyversion => '1.074',
+    my($self)=$class->SUPER::new( yyversion => '1.075',
                                   yyGRAMMAR  =>
 [
   [ _SUPERSTART => '$start', [ 'treeregexplist', '$end' ], 0 ],
